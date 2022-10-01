f4c7db846aaa5994b5fbf38339d92c8d98e3c1db
thetrickuser <coolady1807@outlook.com>
2022-10-01 16:24:54 +0530

    Added button animation

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -2,9 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_catalog/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changeButton = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -19,7 +26,7 @@ class LoginPage extends StatelessWidget {
                 fit: BoxFit.cover,
               ),
               Text(
-                "Welcome",
+                "Welcome $name",
                 style: TextStyle(
                   fontSize: 32,
                 ),
@@ -32,22 +39,52 @@ class LoginPage extends StatelessWidget {
                     TextFormField(
                       decoration: InputDecoration(
                           hintText: 'Enter Username', labelText: 'Username'),
+                      onChanged: ((value) {
+                        name = value;
+                        setState(() {});
+                      }),
                     ),
                     TextFormField(
                       decoration: InputDecoration(
                           hintText: 'Enter Password', labelText: 'Password'),
                       obscureText: true,
                     ),
-                    ElevatedButton(
-                      style: TextButton.styleFrom(),
-                      child: const Center(
-                          child: Text(
-                        "Login",
-                        textDirection: TextDirection.ltr,
-                      )),
-                      onPressed: () {
+                    // ElevatedButton(
+                    //   style: TextButton.styleFrom(),
+                    //   child: const Center(
+                    //       child: Text(
+                    //     "Login",
+                    //     textDirection: TextDirection.ltr,
+                    //   )),
+                    //   onPressed: () {
+                    //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    //   },
+                    // )
+                    InkWell(
+                      onTap: () async {
+                        setState(() {
+                          changeButton = true;
+                        });
+                        await Future.delayed(Duration(seconds: 1));
                         Navigator.pushNamed(context, MyRoutes.homeRoute);
                       },
+                      child: AnimatedContainer(
+                        duration: Duration(seconds: 1),
+                        width: changeButton ? 50 : 150,
+                        height: 50,
+                        alignment: Alignment.center,
+                        child: changeButton
+                            ? Icon(Icons.done, color: Colors.white)
+                            : Text("Login",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18)),
+                        decoration: BoxDecoration(
+                            color: Colors.purple,
+                            borderRadius:
+                                BorderRadius.circular(changeButton ? 50 : 8)),
+                      ),
                     )
                   ],
                 ),
